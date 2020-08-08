05323f5dfe6208a1f03ce7a3268270e4ce585798
GusttaFS <gustavo.silva@ccc.ufcg.edu.br>
2020-08-08 20:16:44 -0300

    Iniciando funcionalidades.

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const proffys = [
+    {name: "Diego Fernandes", 
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4", 
+    whatsapp: "994084200", 
+    bio: "Entusiasta das melhores tecnologias de química avançada. <br><br> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject: "Química", 
+    cost: "20", 
+    weekday: [0], 
+    time_from: [720], 
+    time_to: [1220]}
+]
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function  pageStudy(req, res) {
+    return res.render("study.html", {proffys})
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html")
+}
+
+const express = require('express')
+const server = express()
+
+//configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('scr/views', {
+    express: server,
+    noCache: true,
+})
+
+server
+// configurar arquivos estaticos (css, scripts, imagens)
+.use(express.static("public"))
+// rotas de aplicacao
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.listen(5500)
