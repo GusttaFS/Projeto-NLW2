8b9635b3136564ca991b6f922fbdf2a87e2398e5
GusttaFS <gustavo.silva@ccc.ufcg.edu.br>
2020-08-14 19:53:01 -0300

    trocando render por sendFile.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const proffys = [
-    {name: "Diego Fernandes", 
+    {
+    name: "Diego Fernandes", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4", 
     whatsapp: "994084200", 
     bio: "Entusiasta das melhores tecnologias de química avançada. <br><br> Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
@@ -7,19 +8,20 @@ const proffys = [
     cost: "20", 
     weekday: [0], 
     time_from: [720], 
-    time_to: [1220]}
+    time_to: [1220]
+    }
 ]
 
 function pageLanding(req, res) {
-    return res.render("index.html")
+    return res.sendFile(__dirname + "/views/index.html")
 }
 
 function  pageStudy(req, res) {
-    return res.render("study.html", {proffys})
+    return res.sendFile(__dirname + "/views/study.html", {proffys})
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    return res.sendFile(__dirname + "/views/give-classes.html")
 }
 
 const express = require('express')
@@ -29,7 +31,7 @@ const server = express()
 const nunjucks = require('nunjucks')
 nunjucks.configure('scr/views', {
     express: server,
-    noCache: true,
+    noCache: true
 })
 
 server
